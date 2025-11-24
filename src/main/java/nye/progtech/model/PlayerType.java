2e82604246f323c783d6ecaaa0392b535575f917
MoriczJanos113C <moriczj10@gmail.com>
2025-11-24 10:00:21 +0100

    PlayerType.java checkstyle is done.

--- src/main/java/nye/progtech/model/PlayerType.java
+++ src/main/java/nye/progtech/model/PlayerType.java
@@ -1,6 +1,17 @@
 package nye.progtech.model;
 
+/**
+ * Represents the type of player in the game.
+ */
 public enum PlayerType {
+
+    /**
+     * A human-controlled player.
+     */
     HUMAN,
+
+    /**
+     * A computer-controlled (AI) player.
+     */
     COMPUTER
 }
